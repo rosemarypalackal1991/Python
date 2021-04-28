dd1d9df3676c030db0e69fddb2d36a01aabfeada
rosemarypalackal1991 <78731074+rosemarypalackal1991@users.noreply.github.com>
2021-04-29 00:17:02 +0530

    Create IfElse.py

---
+++ IfElse.py
@@ -0,0 +1,8 @@
+    if n%2 ==1:
+        print("Weird")
+    elif (n%2==0) and (2<= n) and (n<=5):
+        print("Not Weird")
+    elif (n%2==0) and (6<= n <=20):
+        print("Weird")   
+    else:
+        print("Not Weird")
